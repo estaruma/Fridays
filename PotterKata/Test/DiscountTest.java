f2e1bb62189445ba35746bd774302fd105f74d39
estaruma <esther.arumainayagam@mettle.co.uk>
2024-08-09 16:40:42 +0100

    Potter

--- PotterKata/Test/DiscountTest.java
+++ PotterKata/Test/DiscountTest.java
@@ -5,7 +5,7 @@ class DiscountTest {
 
     @Test
     public void NoDiscountTest() {
-        Discount discount = new Discount(0, 0);
+        Discount discount = new Discount();
         double totalPrice = 8.0;
         int numOfDifferentBooks = 1;
 
@@ -15,52 +15,52 @@ class DiscountTest {
 
     @Test
     public void fivePercentDiscountTest() {
-        Discount discount = new Discount(0, 0);
+        Discount discount = new Discount();
         double totalPrice = 16.0;
         int numOfDifferentBooks = 2;
 
         double expectedPrice = 16.0 * 0.95;
-        assertEquals(expectedPrice, discount.applyDiscount(numOfDifferentBooks, totalPrice), 0.0);
+        assertEquals(expectedPrice, discount.applyDiscount(numOfDifferentBooks, totalPrice));
     }
 
     @Test
     public void tenPercentDiscountTest() {
-        Discount discount = new Discount(0, 0);
+        Discount discount = new Discount();
         double totalPrice = 24.0;
         int numOfDifferentBooks = 3;
 
         double expectedPrice = 24.0 * 0.90;
-        assertEquals(expectedPrice, discount.applyDiscount(numOfDifferentBooks, totalPrice), 0.0);
+        assertEquals(expectedPrice, discount.applyDiscount(numOfDifferentBooks, totalPrice));
     }
 
     @Test
     public void twentyPercentDiscountTest() {
-        Discount discount = new Discount(0, 0);
+        Discount discount = new Discount();
         double totalPrice = 32.0;
         int numOfDifferentBooks = 4;
 
         double expectedPrice = 32.0 * 0.80;
-        assertEquals(expectedPrice, discount.applyDiscount(numOfDifferentBooks, totalPrice), 0.0);
+        assertEquals(expectedPrice, discount.applyDiscount(numOfDifferentBooks, totalPrice));
     }
 
     @Test
     public void twentyFivePercentDiscountTest() {
-        Discount discount = new Discount(0, 0);
+        Discount discount = new Discount();
         double totalPrice = 40.0;
         int numOfDifferentBooks = 5;
 
         double expectedPrice = 40.0 * 0.75;
-        assertEquals(expectedPrice, discount.applyDiscount(numOfDifferentBooks, totalPrice), 0.0);
+        assertEquals(expectedPrice, discount.applyDiscount(numOfDifferentBooks, totalPrice));
     }
 
     @Test
     public void fiveOrMoreDiscountTest() {
-        Discount discount = new Discount(0, 0);
+        Discount discount = new Discount();
         double totalPrice = 48.0;
         int numOfDifferentBooks = 6;
 
         double expectedPrice = 48.0;
-        assertEquals(expectedPrice, discount.applyDiscount(numOfDifferentBooks, totalPrice), 0.0);
+        assertEquals(expectedPrice, discount.applyDiscount(numOfDifferentBooks, totalPrice));
     }
 
 
--- PotterKata/src/Discount.java
+++ PotterKata/src/Discount.java
@@ -1,48 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Discount {
     private int numOfBooks;
 
     private double percentage;
 
 
-    // constructor
-    public Discount(int numOfBooks, double percentage) {
-        this.numOfBooks = numOfBooks;
-        this.percentage = percentage;
+
+
+
+    private static final double DISCOUNT_FOR_TWO_BOOKS = 0.05;
+    private static final double DISCOUNT_FOR_THREE_BOOKS = 0.10;
+
+    private static final double DISCOUNT_FOR_FOUR_BOOKS = 0.20;
+
+    private static final double DISCOUNT_FOR_FIVE_BOOKS = 0.25;
+
+
+    // mapping different books to the discount percentage
+    private static final Map<Integer, Double> DISCOUNT_RATES = new HashMap<>();
+
+    // static initialisation block
+    // check for 2,3,4, 5 different books
+    static {
+        DISCOUNT_RATES.put(2, DISCOUNT_FOR_TWO_BOOKS);
+        DISCOUNT_RATES.put(3, DISCOUNT_FOR_THREE_BOOKS);
+        DISCOUNT_RATES.put(4, DISCOUNT_FOR_FOUR_BOOKS);
+        DISCOUNT_RATES.put(5, DISCOUNT_FOR_FIVE_BOOKS);
     }
 
-    // getter for numofBooks
-    public int getNumOfBooks() {
-        return numOfBooks;
-    }
 
-    // getter for discount
-    public double getPercentage() {
-        return percentage;
-    }
-
-
-    // method to apply different discounts
-
+    // calculates and returns the discounted price based on the number of different books
+    // totalPrice x 0.95
     public double applyDiscount(int numOfDifferentBooks, double totalPrice) {
-        double percentage;
-
-        // If, however, you buy two different books from the series, you get a 5% discount on those two books.
-        if (numOfDifferentBooks == 2) {
-            return totalPrice * 0.95;
-            // If you buy 3 different books, you get a 10% discount.
-        } else if (numOfDifferentBooks == 3) {
-            return totalPrice * 0.90;
-            // With 4 different books, you get a 20% discount.
-        } else if (numOfDifferentBooks == 4) {
-            return totalPrice * 0.80;
-            // If you go the whole hog, and buy all 5, you get a huge 25% discount.
-        } else if (numOfDifferentBooks == 5) {
-            return totalPrice * 0.75;
-        } else {
-            return totalPrice;
+        Double discountPercentage = DISCOUNT_RATES.get(numOfDifferentBooks);
+        if (discountPercentage != null) {
+            return totalPrice * (1 - discountPercentage);
         }
-
+        return totalPrice;
     }
-
-
 }
